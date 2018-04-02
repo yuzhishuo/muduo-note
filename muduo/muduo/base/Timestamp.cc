15cf71cacffd8d6d01dc39864cb698039cb2a43c
aoumeior <aoumeior@outlook.com>
2018-04-02 23:18:57 +0800

    增加timestamp.* 注释

---
+++ muduo/muduo/base/Timestamp.cc
@@ -0,0 +1,92 @@
+#include <muduo/base/Timestamp.h>
+
+#include <sys/time.h>
+#include <stdio.h>
+
+// c++使用PRID64，需要两步：
+
+//     包含头文件：<inttypes.h>
+//     定义宏：__STDC_FORMAT_MACROS，可以通过编译时加-D__STDC_FORMAT_MACROS，或者在包含文件之前定义这个宏。
+
+#ifndef __STDC_FORMAT_MACROS // !!!
+#define __STDC_FORMAT_MACROS // !!!
+#endif
+
+#include <inttypes.h>         // !!!
+
+#include <boost/static_assert.hpp>
+
+using namespace muduo;
+
+BOOST_STATIC_ASSERT(sizeof(Timestamp) == sizeof(int64_t));
+
+string Timestamp::toString() const
+{
+  char buf[32] = {0};
+  int64_t seconds = microSecondsSinceEpoch_ / kMicroSecondsPerSecond;
+  int64_t microseconds = microSecondsSinceEpoch_ % kMicroSecondsPerSecond;
+  // int64_t用来表示64位整数，在32位系统中是long long int，在64位系统中是long int，所以打印int64_t的格式化方法是：
+  // 源文件中 sceonds,microseconds 定义为 int64_t
+  snprintf(buf, sizeof(buf)-1, "%" PRId64 ".%06" PRId64 "", seconds, microseconds);
+  // snprintf 将可变个参数(...)按照format格式化成字符串，然后将其复制到str中
+  return buf;
+}
+
+//  你看默认的时间戳中定义的是毫秒级的
+string Timestamp::toFormattedString(bool showMicroseconds) const
+{
+  char buf[32] = {0};
+  time_t seconds = static_cast<time_t>(microSecondsSinceEpoch_ / kMicroSecondsPerSecond);
+  
+  struct tm tm_time;
+  // struct tm *gmtime(const time_t *timep)；
+
+  // struct tm *gmtime_r(const time_t *timep, struct tm *result);
+
+  // 这两个函数意思一样，将timep这个秒数转换成以UTC时区为标准的年月日时分秒时间。gmtime_r是线程安全的，
+  gmtime_r(&seconds, &tm_time);
+
+  if (showMicroseconds)
+  {
+    int microseconds = static_cast<int>(microSecondsSinceEpoch_ % kMicroSecondsPerSecond);
+    snprintf(buf, sizeof(buf), "%4d%02d%02d %02d:%02d:%02d.%06d",
+             tm_time.tm_year + 1900, tm_time.tm_mon + 1, tm_time.tm_mday,
+             tm_time.tm_hour, tm_time.tm_min, tm_time.tm_sec,
+             microseconds);
+  }
+  else
+  {
+    snprintf(buf, sizeof(buf), "%4d%02d%02d %02d:%02d:%02d",
+             tm_time.tm_year + 1900, tm_time.tm_mon + 1, tm_time.tm_mday,
+             tm_time.tm_hour, tm_time.tm_min, tm_time.tm_sec);
+  }
+  return buf;
+}
+
+//  timeval
+//  {
+//    time_t tv_sec; //秒 [long int]
+//    suseconds_t tv_usec; //微秒 [long int]
+//  };
+
+Timestamp Timestamp::now()
+{
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  int64_t seconds = tv.tv_sec;
+  return Timestamp(seconds * kMicroSecondsPerSecond + tv.tv_usec);
+}
+
+// struct tm {
+//   int tm_sec; /* 秒 – 取值区间为[0,59] */
+//   int tm_min; /* 分 - 取值区间为[0,59] */
+//   int tm_hour; /* 时 - 取值区间为[0,23] */
+//   int tm_mday; /* 一个月中的日期 - 取值区间为[1,31] */
+//   int tm_mon; /* 月份（从一月开始，0代表一月） - 取值区间为[0,11] */
+//   int tm_year; /* 年份，其值等于实际年份减去1900 */
+//   int tm_wday;  /* 星期 – 取值区间为[0,6]，其中0代表星期天，1代表星期一，以此类推 */ 
+//   int tm_yday; /* 从每年的1月1日开始的天数 – 取值区间为[0,365]，其中0代表1月1日，1代表1月2日，以此类推 */
+//   int tm_isdst; /* 夏令时标识符，实行夏令时的时候，tm_isdst为正。不实行夏令时的时候，tm_isdst为0；不了解情况时，tm_isdst()为负。*/
+//   long int tm_gmtoff; /*指定了日期变更线东面时区中UTC东部时区正秒数或UTC西部时区的负秒数*/
+//   const char *tm_zone; /*当前时区的名字(与环境变量TZ有关)*/
+// };
---
+++ muduo/muduo/base/Timestamp.h
@@ -0,0 +1,126 @@
+#ifndef MUDUO_BASE_TIMESTAMP_H
+#define MUDUO_BASE_TIMESTAMP_H
+
+#include <muduo/base/copyable.h>
+#include <muduo/base/Types.h>
+
+#include <boost/operators.hpp>
+
+namespace muduo
+{
+
+///
+/// Time stamp in UTC, in microseconds resolution.
+///
+/// This class is immutable.
+/// It's recommended to pass it by value, since it's passed in register on x64.
+///
+class Timestamp : public muduo::copyable,
+                  // 
+                  // 要派生自 boost::equality_comparable, 派生类(T)必须提供：
+
+                  // bool operator==(const T&,const T&);
+
+                  // 同样，返回值的类型不必是 bool, 但必须可以隐式转换为 bool. C++标准中的概念 EqualityComparable 要求必须提供 operator== ，因此从 equality_comparable 派生的类必须符合该要求。equality_comparable 类为 T 提供 bool operator!=(const T&,const T&).
+                  public boost::equality_comparable<Timestamp>,
+                  // boost::less_than_comparable
+                  // 继承使用此类可以只重写operator< ，<= ,>会自动生成
+                  public boost::less_than_comparable<Timestamp>
+{
+ public:
+  ///
+  /// Constucts an invalid Timestamp.
+  ///
+  Timestamp()
+    : microSecondsSinceEpoch_(0)
+  {
+  }
+
+  ///
+  /// Constucts a Timestamp at specific time
+  ///
+  /// @param microSecondsSinceEpoch
+  explicit Timestamp(int64_t microSecondsSinceEpochArg)
+    : microSecondsSinceEpoch_(microSecondsSinceEpochArg)
+  {
+  }
+
+  void swap(Timestamp& that)
+  {
+    std::swap(microSecondsSinceEpoch_, that.microSecondsSinceEpoch_);
+  }
+
+  // default copy/assignment/dtor are Okay
+
+  string toString() const;
+  string toFormattedString(bool showMicroseconds = true) const;
+
+  bool valid() const { return microSecondsSinceEpoch_ > 0; }
+
+  // for internal usage.
+  // time_t实际上就是长整型long int
+  int64_t microSecondsSinceEpoch() const { return microSecondsSinceEpoch_; }
+  time_t secondsSinceEpoch() const
+  { return static_cast<time_t>(microSecondsSinceEpoch_ / kMicroSecondsPerSecond); }
+
+  ///
+  /// Get time of now.
+  ///
+  static Timestamp now();
+  static Timestamp invalid()
+  {
+    return Timestamp();
+  }
+
+  static Timestamp fromUnixTime(time_t t)
+  {
+    return fromUnixTime(t, 0);
+  }
+
+  static Timestamp fromUnixTime(time_t t, int microseconds)
+  {
+    return Timestamp(static_cast<int64_t>(t) * kMicroSecondsPerSecond + microseconds);
+  }
+
+  static const int kMicroSecondsPerSecond = 1000 * 1000;
+
+ private:
+  int64_t microSecondsSinceEpoch_;
+};
+
+inline bool operator<(Timestamp lhs, Timestamp rhs)
+{
+  return lhs.microSecondsSinceEpoch() < rhs.microSecondsSinceEpoch();
+}
+
+inline bool operator==(Timestamp lhs, Timestamp rhs)
+{
+  return lhs.microSecondsSinceEpoch() == rhs.microSecondsSinceEpoch();
+}
+
+///
+/// Gets time difference of two timestamps, result in seconds.
+///
+/// @param high, low
+/// @return (high-low) in seconds
+/// @c double has 52-bit precision, enough for one-microsecond
+/// resolution for next 100 years.
+inline double timeDifference(Timestamp high, Timestamp low)
+{
+  int64_t diff = high.microSecondsSinceEpoch() - low.microSecondsSinceEpoch();
+  return static_cast<double>(diff) / Timestamp::kMicroSecondsPerSecond;
+}
+
+///
+/// Add @c seconds to given timestamp.
+///
+/// @return timestamp+seconds as Timestamp
+///
+inline Timestamp addTime(Timestamp timestamp, double seconds)
+{
+  int64_t delta = static_cast<int64_t>(seconds * Timestamp::kMicroSecondsPerSecond);
+  return Timestamp(timestamp.microSecondsSinceEpoch() + delta);
+}
+
+}
+#endif  // MUDUO_BASE_TIMESTAMP_H
