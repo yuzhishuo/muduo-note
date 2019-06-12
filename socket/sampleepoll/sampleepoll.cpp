cf7dee23714bfd0fd4b0c14deafe9efd6ee06197
aoumeior <aoumeior@outlook.com>
2019-06-12 09:18:48 +0800

    fix

--- socket/sampleepoll/sampleepoll.cpp
+++ socket/sampleepoll/sampleepoll.cpp
@@ -1,30 +1,29 @@
-#include <signal.h>
+#include <sys/types.h>
+#include <sys/epoll.h> // for epoll, 
 #include <sys/wait.h>
-
-#include <poll.h>
-#include <netinet/in.h>
-#include <stdlib.h>
-#include <error.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <signal.h>
+#include <errno.h>
 #include <string.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <algorithm>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <arpa/inet.h> // inet_ntoa,
+#include <unistd.h>
 #include <vector>
 #include <iostream>
-#include <arpa/inet.h>
-#include <error.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-
-#define ERR_EXIT(n) \
-    do \
-    {   \
-        perror(n);  \       
+#define ERR_EXIT(n)         \
+    do                      \
+    {                       \
+        perror(n);          \
         exit(EXIT_FAILURE); \
-    }while(0)
+    } while (0)
 
-typedef std::vector<struct pollfd> PollFdList;
+typedef std::vector<struct epoll_event> EventList;
 
-int main ()
+int main()
 {
     signal(SIGPIPE, SIG_IGN);
     signal(SIGCHLD, SIG_IGN);
@@ -34,96 +33,100 @@ int main ()
     // socket(AF_INET, SOCK_STREAM, 0)
     if ((listenfd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, IPPROTO_TCP)) < 0)
         ERR_EXIT("SOCKET");
-    
+
     struct sockaddr_in servaddr;
     memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family =  AF_INET ;
+    servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(5000);
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
     int on = 1;
 
-    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0 )
+    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0)
         ERR_EXIT("set sockopt");
 
-    if (bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0)
+    if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
         ERR_EXIT("LISTEN");
-    
-    if(listen(listenfd, 5) < 0)
-        ERR_EXIT("bind");
-    
-    struct pollfd pfd;
-    pfd.fd = listenfd;
-    pfd.events = POLLIN; 
 
-    PollFdList pollfds;
-    pollfds.push_back(pfd);
+    if (listen(listenfd, 5) < 0)
+        ERR_EXIT("bind");
+
+    std::vector<int> clients;
+    int epollfd = epoll_create1(EPOLL_CLOEXEC);
+
+    struct epoll_event event;
+    event.data.fd = listenfd;
+    event.events = EPOLLIN /* | EPOLLET */;
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, listenfd, &event);
+
+    EventList events{16};
 
     int nready;
-
     int connfd;
     struct sockaddr_in peeraddr;
     socklen_t peerlen;
 
-    while(1)
+    while (true)
     {
-        nready = poll(&*pollfds.begin(),pollfds.size(), /* 超时时间 */ -1);
-        if( nready == -1 )
+        nready = epoll_wait(epollfd, &*events.begin(), events.size(), -1);
+
+        if (nready == -1)
         {
-            if(errno == EINTR)
+            if (errno == EINTR)
                 continue;
+            ERR_EXIT("epoll wait");
         }
-        if( nready == 0)
-        {
+        if (nready == 0)
             continue;
-        }
-        // 判断是否有监听事件到来
-        if( pollfds[0].revents & POLLIN)
+        if (nready == events.size())
+            events.resize(events.size() * 2);
+
+        for (int i = 0; i < events.size(); ++i)
         {
-            peerlen = sizeof(peeraddr);
-                    // 如果有则 接受返回一个套接字
-            connfd = ::accept4(listenfd, (struct sockaddr*)&peeraddr, &peerlen,
-                                SOCK_NONBLOCK | SOCK_CLOEXEC );
-
-            if(connfd == -1)
+            if (events[i].data.fd == listenfd)
             {
-                ERR_EXIT("accept4");
-            }
-  
-            std::cout << "aleard connect" << std::endl;
-            // 加入到监听，连接
-            pfd.fd = connfd;
-            pfd.events = POLLIN;
-            pfd.revents = 0;
-            pollfds.push_back(pfd);
-            --nready;
+                peerlen = sizeof(peeraddr);
+                connfd = ::accept4(listenfd, (struct sockaddr*)&peeraddr, &peerlen,
+                                SOCK_CLOEXEC |SOCK_NONBLOCK);
+                if(connfd == -1)
+                {
+                    // EMFILE PROCESS
+                    if(errno == EMFILE)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        ERR_EXIT("epoll accept4");
+                    }
+                }
 
-            if(nready == 0)
-                continue;
-        }
+                std::cout << "ip:" << inet_ntoa(peeraddr.sin_addr) << "prot:" << ntohs(peeraddr.sin_port) << std::endl;
 
-        // 遍历已经连接套接字
-        for(auto it = pollfds.begin()+1; it < pollfds.end() and nready > 0; it++)
-        {
-            if(it->events && POLLIN)
+                clients.push_back(connfd);
+                event.data.fd = connfd;
+                event.events = EPOLLIN;
+                epoll_ctl(epollfd, EPOLL_CTL_ADD, connfd, &event);
+            }else if( events[i].events & EPOLLIN)
             {
-                --nready;
-                connfd = it->fd;
-                char buf[1024] = {0};
-                int ret = read(connfd, buf, 1024);
+                connfd =  events[i].data.fd;
+                if(connfd < 0) continue;
+                
+                char buf[20] = {0};
+                int ret = read(connfd, buf, 20);
 
-                if(ret == -1)
+                if (ret == -1)
                 {
                     ERR_EXIT("read");
                 }
 
                 // 判断对方是否关闭套接字
-                if(ret == 0)
+                if (ret == 0)
                 {
                     std::cout << "client close" << std::endl;
-                    it = pollfds.erase(it);
-                    --it;
                     close(connfd);
+                    epoll_ctl(epollfd, EPOLL_CTL_DEL, connfd, &event);
+                    clients.erase(std::remove(clients.begin(), clients.end(), connfd), clients.end());
                     continue;
                 }
                 std::cout << buf;
@@ -131,5 +134,6 @@ int main ()
                 write(connfd, buf, strlen(buf));
             }
         }
+        
     }
 }
